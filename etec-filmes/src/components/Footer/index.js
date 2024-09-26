4dd0de6b1a5f673b06d0dc0711aa2f26b18e4767
BrunoSorrilha <bruninhodepa10@gmail.com>
2024-09-26 08:35:42 -0300

    gargula

---
+++ etec-filmes/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> EtecFlix &copy; Desenvolvido por Bruno Sorrilha</h2>
+        </footer>
+    );
+}
+
+export default Footer;
